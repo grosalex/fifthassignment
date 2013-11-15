ae0490b768572bb4993052a7503b38356208d806
grosalex <alexbruneau93@gmail.com>
2013-11-14 20:37:27 -0500

    initial commit

---
+++ fifthAssignment.cpp
@@ -0,0 +1,29 @@
+/*
+ ============================================================================
+ Name        : fifthAssignment.cpp
+ Author      : grosalex
+ Version     :
+ Copyright   : do what you want with it
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "This is the fifth Assignment" << endl; /* prints This is the fifth Assignment */
+	/*
+	 * TODO
+	 * client method => withdrawal, deposite, transfertmoney(acount source, account target, value)
+	 *
+	 * 1) add/closing branch
+	 * 2) add/remove clients dans une branche
+	 * 3) open account for a client
+	 * 4) recherche client
+	 * 5) show clients from a branch
+	 * 6) transaction between two date
+	 */
+	return 0;
+}
